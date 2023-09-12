f86f97028b5518ae78784f34b9644fd0ef6b47cf
Rosen Santev <r.ssantev@gmail.com>
2023-09-12 17:58:52 +0300

    wip re-architecture

---
+++ contracts/Hydra/ValidatorSet/modules/Staking/LiquidStaking.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "./ILiquidStaking.sol";
+import "./../../ValidatorSetBase.sol";
+import "./../../../LiquidityToken/ILiquidityToken.sol";
+import "./../../libs/ValidatorStorage.sol";
+
+abstract contract LiquidStaking is ILiquidStaking, ValidatorSetBase {
+    using ValidatorStorageLib for ValidatorTree;
+
+    // Liquid Staking token given to stakers and delegators
+    address internal _liquidToken;
+
+    function liquidToken() external view override returns (address) {
+        return _liquidToken;
+    }
+
+    function _onStake(address staker, uint256 stakedAmount) internal {
+        _mintTokens(staker, stakedAmount);
+    }
+
+    function _onUnstake(address staker, uint256 unstakedAmount) internal {
+        // User needs to burn the liquid tokens for slashed stake as well
+        uint256 liquidDebt = _validators.get(msg.sender).liquidDebt;
+        if (liquidDebt > 0) {
+            _validators.get(msg.sender).liquidDebt = 0;
+            unstakedAmount += liquidDebt;
+        }
+
+        _burnTokens(staker, unstakedAmount);
+    }
+
+    function _onDelegate(address delegator, uint256 stakedAmount) internal {
+        _mintTokens(delegator, stakedAmount);
+    }
+
+    function _onUndelegate(address delegator, uint256 unstakedAmount) internal {
+        _burnTokens(delegator, unstakedAmount);
+    }
+
+    function _mintTokens(address account, uint256 amount) private {
+        ILiquidityToken(_liquidToken).mint(account, amount);
+    }
+
+    function _burnTokens(address account, uint256 amount) private {
+        ILiquidityToken(_liquidToken).burn(account, amount);
+    }
+}
