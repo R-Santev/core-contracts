bfca32ca34ff2c5a866a5b6dfd493d1d06f37328
Vitomir Pavlov <vitomirpavlov@gmail.com>
2023-12-06 08:17:28 +0200

    delete redundant mock properties and leave a todo comment;

--- contracts/mocks/MockValidatorSet.sol
+++ contracts/mocks/MockValidatorSet.sol
@@ -9,13 +9,11 @@ import "./../Hydra/ValidatorSet/ValidatorSet.sol";
  * It just adds read functions to check the state of the ValidatorSet contract
  */
 contract MockValidatorSet is ValidatorSet {
-    // using ValidatorStorageLib for ValidatorTree;
-    // using ValidatorQueueLib for ValidatorQueue;
-    // using RewardPoolLib for RewardPool;
     using SafeMathUint for uint256;
 
     function getRawDelegatorReward(address validator, address delegator) external view returns (uint256) {
-        // return validators.getDelegationPool(validator).claimableRewards(delegator);
+        // TODO: get the claimable rewards
+        // old code: return validators.getDelegationPool(validator).claimableRewards(delegator);
         return 0;
     }
 }
