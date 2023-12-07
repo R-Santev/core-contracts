c3b91748895e1b5a53a18fa0edf9cd13bb897957
Vitomir Pavlov <vitomirpavlov@gmail.com>
2023-12-07 16:16:03 +0200

    [OPTIMIZE] optimize the current architechture with a better one - use context and fixtures combined;
    rebase;

--- test/Hydra/mochaContext.ts
+++ test/Hydra/mochaContext.ts
@@ -1,10 +1,14 @@
 /* eslint-disable node/no-extraneous-import */
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { BigNumber, Contract } from "ethers";
 =======
 import { BigNumber } from "ethers";
 >>>>>>> 232c201 (re-structure the tests - create mocha context, create a test that initializing the context and separate each fixture to be responsible for 1 contract;)
+=======
+import { BigNumber, Contract } from "ethers";
+>>>>>>> f7701b1 ([OPTIMIZE] optimize the current architechture with a better one - use context and fixtures combined;)
 
 export interface Signers {
   accounts: SignerWithAddress[];
@@ -15,6 +19,9 @@ export interface Signers {
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> f7701b1 ([OPTIMIZE] optimize the current architechture with a better one - use context and fixtures combined;)
 export interface Fixtures {
   validatorSetFixture: { (): Promise<Contract> };
 }
@@ -22,10 +29,13 @@ export interface Fixtures {
 declare module "mocha" {
   export interface Context {
     fixtures: Fixtures;
+<<<<<<< HEAD
 =======
 declare module "mocha" {
   export interface Context {
 >>>>>>> 232c201 (re-structure the tests - create mocha context, create a test that initializing the context and separate each fixture to be responsible for 1 contract;)
+=======
+>>>>>>> f7701b1 ([OPTIMIZE] optimize the current architechture with a better one - use context and fixtures combined;)
     signers: Signers;
     epochReward: BigNumber;
     minStake: BigNumber;
