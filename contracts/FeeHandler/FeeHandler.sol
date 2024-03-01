71a2a4ff0f25f48c1a6dd47f2230a98b853795b6
Vitomir Pavlov <vitomirpavlov@gmail.com>
2024-03-01 10:47:43 +0200

    delete the _ prefix

--- contracts/FeeHandler/FeeHandler.sol
+++ contracts/FeeHandler/FeeHandler.sol
@@ -18,11 +18,11 @@ contract FeeHandler is Initializable, OwnableUpgradeable {
 
     /**
      * @notice Generic method that will be used to transfer the generated fees to another contract
-     * @param _contractAddress The address of the contract that will be called
-     * @param _encodedFunction The encoded function with its signature and parameters, if any
+     * @param contractAddress The address of the contract that will be called
+     * @param encodedFunction The encoded function with its signature and parameters, if any
      */
-    function relocateFees(address _contractAddress, bytes memory _encodedFunction) public onlyOwner {
-        (bool success, bytes memory data) = _contractAddress.call{value: address(this).balance}(_encodedFunction);
+    function relocateFees(address contractAddress, bytes memory encodedFunction) public onlyOwner {
+        (bool success, bytes memory data) = contractAddress.call{value: address(this).balance}(encodedFunction);
 
         emit Response(success, data);
     }
