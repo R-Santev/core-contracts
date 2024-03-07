45cc340bd496ed6170b11844a3c3af575d94481e
Vitomir Pavlov <vitomirpavlov@gmail.com>
2024-03-07 11:07:26 +0200

    Issue-47 and issue-49 (#38)
    * delete the redundant Vesting directory with the Vesting.sol from the common directory and set default APR indexes;

--- contracts/RewardPool/modules/APR.sol
+++ contracts/RewardPool/modules/APR.sol
@@ -9,9 +9,9 @@ contract APR is Initializable, AccessControl {
     uint256 public constant EPOCHS_YEAR = 31500;
     bytes32 public constant MANAGER_ROLE = keccak256("manager_role");
 
-    uint256 public base;
-    uint256 public macroFactor;
-    uint256 public rsi;
+    uint256 public base = 500;
+    uint256 public macroFactor = 7500;
+    uint256 public rsi = 10000;
     uint256[52] public vestingBonus;
 
     function __APR_init(address manager) internal onlyInitializing {
